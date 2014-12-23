a81e08c63c99d7201a30cb4633046aa184db1aa4
surr <litichevskij.vova@gmail.com>
2014-12-23 05:31:39 +0200

    config.example is updated

--- config.example.js
+++ config.example.js
@@ -1,5 +1,10 @@
 module.exports = {
 
+    theServerPort : 7000,
+
+    maxTimeToWait : 60000, // a minute
+    pingInterval  : 10000, // 10 seconds
+
     appDirectCredentials : {
         consumer_key     : 'a53c05bb316a',
         consumer_secret  : '53d1381ec71f3ce1f238ab2c'
@@ -14,7 +19,7 @@ module.exports = {
             }
         },
         s2 : {
-            address : 'http://localhost:3000',
+            address : 'http://other.host',
             credentials : {
                 consumer_key    : 'a53c05bb316a',
                 consumer_secret : '53d1381ec71f3ce1f238ab2c'
