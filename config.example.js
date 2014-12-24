e122fd6626f4f0db9d20ae77d773795651696958
surr <litichevskij.vova@gmail.com>
2014-12-25 00:58:03 +0200

    comments at config.example

--- config.example.js
+++ config.example.js
@@ -2,7 +2,11 @@ module.exports = {
 
     theServerPort : 7000,
 
+    // how long to wait anactive server for an answer
+    // before send 500 to appDirect
     maxTimeToWait : 60000, // a minute
+
+    // how often to send ping request to a unactive server
     pingInterval  : 10000, // 10 seconds
 
     appDirectCredentials : {
@@ -11,14 +15,19 @@ module.exports = {
     },
 
     servers : {
-        s1 : {
-            address : 'http://localhost:3000',
+
+        // ensure that server address is visible from the Internet
+        // DO NOT use nothing like localhost, 127.0.0.1 or
+        // a local address behind a NAT like 10.10.0.100 etc.
+        one : {
+            address : 'http://example.com:3000',
             credentials : {
                 consumer_key    : 'a53c05bb316a',
                 consumer_secret : '53d1381ec71f3ce1f238ab2c'
             }
         },
-        s2 : {
+
+        two : {
             address : 'http://other.host',
             credentials : {
                 consumer_key    : 'a53c05bb316a',
